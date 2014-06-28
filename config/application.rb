6d7ac6afe20c203a22566bc51fa652dd63a0999f
Patrick Brown <patrick@gravityswitch.com>
2014-06-28 15:59:02 -0400

    serve static assets

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module CooperRails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically
-    loaded.config.serve_static_assets = true
+    # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
