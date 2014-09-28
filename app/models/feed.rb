370d8466bd41c2119fdd6f5dfe1e3512041cca4e
Patrick Brown <patrick.arthur.brown@gmail.com>
2014-09-28 15:13:43 -0400

    tdd out #update_meta and #save_articles

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,4 +4,20 @@ class Feed < ActiveRecord::Base
   validates_presence_of(:title)
   validates_presence_of(:url)
 
+  def update_meta
+    feed = Feedjira::Feed.fetch_and_parse(url)
+    self.update_attributes!(title: feed.title)
+  end
+
+  def save_articles
+    feed = Feedjira::Feed.fetch_and_parse(url)
+    
+    feed.entries.each do |entry|
+      article = Article.new(title: entry.title,
+                            url: entry.url,
+                            summary: entry.summary)
+      article.save!
+    end
+  end
+
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Feed, :type => :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :url }
 
+  describe "#update_meta" do
+    it "should update title" do
+      feed = Feed.new(title: "foo", url: "https://xkcd.com/rss.xml")
+      feed.update_meta
+      expect(feed.title).to eq("xkcd.com")
+    end
+  end
+
+  describe "#save_articles" do
+    it "should send new message to Article for each entry" do
+      feed = Feed.new(title: "xkcd.com", url: "https://xkcd.com/rss.xml")
+      feed.save_articles
+
+      expect(Article.all.count).to_not eq(0)
+      expect(Article.all.count).to_not eq(nil)
+    end
+  end
 end
