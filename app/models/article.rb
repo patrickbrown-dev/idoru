7f10d5e124e0a6989eae783f81c6bbd1493f9635
Patrick Brown <patrick.arthur.brown@gmail.com>
2014-10-15 20:09:30 -0400

    white space

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,9 +3,9 @@ class Article < ActiveRecord::Base
   # Associations -------------------------------------------------------
 
   belongs_to :feed
-  
+
   # Validations --------------------------------------------------------
-  
+
   validates_presence_of :title
   validates_presence_of :url
   validates_presence_of :full_story
