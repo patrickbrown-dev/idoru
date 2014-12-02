ae8245e90309e9de513938f081f4951f8171d9e4
Patrick Brown <patrick.arthur.brown@gmail.com>
2014-12-02 00:49:37 -0500

    route to articles if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,18 @@
 class WelcomeController < ApplicationController
   layout 'welcome'
   def index
+    if user_signed_in?
+      redirect_to test
+    end
+  end
+
+  private
+
+  def test
+    if current_user.is_admin?
+      admin_feeds_path
+    else
+      articles_path
+    end
   end
 end
