8d3e405054b3ec1bcaa5669810af33c6893dece8
Patrick Brown <patrick.arthur.brown@gmail.com>
2014-12-07 03:19:49 -0500

    now this

--- app/assets/javascripts/reader.js
+++ app/assets/javascripts/reader.js
@@ -1,5 +1,3 @@
 //= require ./react.min.js
-//= require ./out/goog/base.js
 //= require reader_app
 
-goog.require("cooper_cljs.dev");
--- vendor/assets/javascripts/out/reagent/interop.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 0.0-2371
-goog.provide('reagent.interop');
-goog.require('cljs.core');
-reagent.interop.fvars = {};
-
-//# sourceMappingURL=interop.js.map
