81c98b8d0aa8a1648313b09f7b37d116bd80f91e
Patrick Brown <patrick.arthur.brown@gmail.com>
2014-12-09 18:24:10 -0500

    deploy from ci

--- deploy_prod.sh
+++
@@ -1,3 +0,0 @@
-ssh -t root@104.236.46.71 "cd /home/rails/idoru && RAILS_ENV=production rake db:migrate"
-ssh -t root@104.236.46.71 "cd /home/rails/idoru && RAILS_ENV=production rake assets:precompile"
-ssh -t root@104.236.46.71 "service unicorn restart"
