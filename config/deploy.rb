e715ef754fda3671ec3b0f1a1c7377e991c5fe97
Patrick Brown <patrick.arthur.brown@gmail.com>
2014-12-09 02:16:46 -0500

    update cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,10 +9,10 @@ set :branch, 'master'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/rails/idoru'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -44,9 +44,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+      #   execute :rake, 'db:migrate:status'
+      end
     end
   end
 
