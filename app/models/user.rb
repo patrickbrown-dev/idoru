d20da49ca20f2e45657a83ed0dfa2db6cfce9720
Patrick Brown <patrick.arthur.brown@gmail.com>
2014-12-26 00:46:17 -0500

    spec fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Associations -------------------------------------------------------
-
+  has_many :subscriptions
   has_many :feeds, through: :subscriptions
-
-  # Instance Methods ---------------------------------------------------
-
-  def create_feed(url)
-    Feed.create_and_update(url: url, user: self)
-  end
 end
