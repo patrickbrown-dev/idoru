56384ce8a0be455c615466397df5a9114337defd
Patrick Brown <patrick.arthur.brown@gmail.com>
2015-03-02 19:08:48 -0500

    stop updating feeds in request cycle

--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -9,7 +9,6 @@ class ReaderController < ApplicationController
 
   def get_articles
     @feeds = Subscription.feeds_for_user(current_user)
-    Feed.update_feeds_concurrently(@feeds)
     Article.where(feed_id: @feeds.map(&:id)).
       paginate(page: params[:page], per_page: 25).
       order(published_at: :desc)
