f62cb7f9f560dfc3c5cc665ccbe83ec95a793c51
Patrick Brown <patrick.arthur.brown@gmail.com>
2015-03-05 01:00:38 -0500

    io for crawler

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -1,9 +1,21 @@
 class Crawler
-  def self.crawl
-    Feed.update_feeds_concurrently(feeds_to_update)
-  end
+  class << self
+    def crawl
+      io "Crawl started"
+      Feed.update_feeds_concurrently(feeds_to_update)
+      io "Crawl finished successfully"
+    rescue => e
+      io "Crawl finished with error: #{e}"
+    end
 
-  def self.feeds_to_update
-    Feed.order(updated_at: :desc).limit(50)
+    private
+
+    def feeds_to_update
+      Feed.order(updated_at: :desc).limit(50)
+    end
+
+    def io message
+      puts "#{Time.zone.now} :: #{message}"
+    end
   end
 end
